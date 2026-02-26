e108057067c72fe9ab9f8de045d417d857451358
Hleb Shauchenka <me@marleb.org>
2026-02-26 14:55:19 +0100

    Initial Commit

---
+++ Queens/QueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueensApp.swift
+//  Queens
+//
+//  Created by Personal on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct QueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
