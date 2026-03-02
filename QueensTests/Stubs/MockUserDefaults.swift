d2c922c7285aae04c70f7b562bcdeb759fc38bac
Hleb Shauchenka <me@marleb.org>
2026-03-02 16:17:29 +0100

    Remove unused func

--- QueensTests/Stubs/MockUserDefaults.swift
+++ QueensTests/Stubs/MockUserDefaults.swift
@@ -11,15 +11,7 @@ final class MockUserDefaults: UserDefaults {
     storage[defaultName]
   }
 
-  override func double(forKey defaultName: String) -> Double {
-    storage[defaultName] as? Double ?? 0
-  }
-
   override func set(_ value: Any?, forKey defaultName: String) {
-    if let value {
-      storage[defaultName] = value
-    } else {
-      storage.removeValue(forKey: defaultName)
-    }
+    storage[defaultName] = value
   }
 }
