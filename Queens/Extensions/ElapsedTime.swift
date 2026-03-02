ec3b82cf999324919ac92ec362e8b7e0c0880505
Hleb Shauchenka <me@marleb.org>
2026-03-02 18:48:46 +0100

    Fix negative elapsed time

--- Queens/Extensions/ElapsedTime.swift
+++ Queens/Extensions/ElapsedTime.swift
@@ -2,13 +2,15 @@ import Foundation
 
 extension TimeInterval {
   func formattedElapsedTime(fractionalSecondsLength: Int = 2) -> String {
-    Duration.seconds(self).formatted(
-      .time(
-        pattern: .minuteSecond(
-          padMinuteToLength: 2,
-          fractionalSecondsLength: fractionalSecondsLength
+    Duration
+      .seconds(max(self, 0))
+      .formatted(
+        .time(
+          pattern: .minuteSecond(
+            padMinuteToLength: 2,
+            fractionalSecondsLength: fractionalSecondsLength
+          )
         )
       )
-    )
   }
 }
